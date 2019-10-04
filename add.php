8e0269735838d76118f41990829217b4c87eb6c9
Ra0l <45242019+Ra0l@users.noreply.github.com>
2019-10-04 17:31:52 -0500

    Tercer commit

--- add.php
+++ add.php
@@ -13,6 +13,7 @@ Email	 	 : info@obedalvarado.pw
 -->
 
 <!-- Habla raul -->
+<!-- No entender nada weon -->
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
