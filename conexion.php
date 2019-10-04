eefa5a9679e7e771f9b8cac6068d7dc31ccb5a09
Ra0l <45242019+Ra0l@users.noreply.github.com>
2019-10-04 18:35:48 -0500

    cambio en conexion

--- conexion.php
+++ conexion.php
@@ -4,7 +4,7 @@ $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
 $db_name = "test_empleados";
-
+// estamos ready
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
 if(mysqli_connect_errno()){
